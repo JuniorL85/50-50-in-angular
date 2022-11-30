a4b559ee4ffadbafc628be973d989460a7273d32
JuniorL85 <albertinolopes@hotmail.com>
2022-11-30 08:08:29 +0100

    la till transition på startsida och ny skarpare bild

--- src/app/components/start-page/start-page.component.ts
+++ src/app/components/start-page/start-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit {
+
+  titleEl: any;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.titleEl = this.elRef.nativeElement.querySelector('h1');
+
+    setTimeout(() => {
+      this.titleEl.classList.add('show');
+    }, 1000);
+  }
 
   title = '50 DAYS 50 PROJECTS';
 
