915909299c0703cec511a38be592517d37f46c59
JuniorL85 <albertinolopes@hotmail.com>
2022-11-30 07:49:08 +0100

    added a start-page

---
+++ src/app/components/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.scss']
+})
+export class StartPageComponent {
+
+  title = '50 DAYS 50 PROJECTS';
+
+}
