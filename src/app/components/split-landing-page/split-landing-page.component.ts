c21f218222ab0e7a72a3ee1b5c7fba0d7673f213
JuniorL85 <albertinolopes@hotmail.com>
2022-12-01 07:11:45 +0100

    split-landing är klar

--- src/app/components/split-landing-page/split-landing-page.component.ts
+++ src/app/components/split-landing-page/split-landing-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-split-landing-page',
   templateUrl: './split-landing-page.component.html',
   styleUrls: ['./split-landing-page.component.scss']
 })
-export class SplitLandingPageComponent {
+export class SplitLandingPageComponent implements OnInit {
+
+  containerEl: any;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.containerEl = this.elRef.nativeElement.querySelector('.container');
+  }
+
+  onMouseEnter(pageSide: string) {
+    if (pageSide === 'left') {
+      this.containerEl.classList.add('hover-left');
+    } else if (pageSide === 'right') {
+      this.containerEl.classList.add('hover-right');
+    }
+  }
+
+  onMouseLeave(pageSide: string) {
+    if (pageSide === 'left') {
+      this.containerEl.classList.remove('hover-left');
+    } else if (pageSide === 'right') {
+      this.containerEl.classList.remove('hover-right');
+    }
+  }
 
 }
