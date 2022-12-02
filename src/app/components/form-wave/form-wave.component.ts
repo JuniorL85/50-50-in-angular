4a3e14486aeb9c72fc5cf66e5576f27212169e5a
JuniorL85 <albertinolopes@hotmail.com>
2022-12-02 11:50:52 +0100

    form-wave tillagd (ej fungerande)

--- src/app/components/form-wave/form-wave.component.ts
+++ src/app/components/form-wave/form-wave.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-wave',
   templateUrl: './form-wave.component.html',
   styleUrls: ['./form-wave.component.scss']
 })
-export class FormWaveComponent {
+export class FormWaveComponent implements OnInit {
+
+  labelEl: any;
+  spanEl: any;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngOnInit(): void {
+    const labels = this.elRef.nativeElement.querySelectorAll('.form-control label')
+
+    console.log('innan ', labels)
+
+    labels.forEach((label: any) => {
+      label.innerHTML = label.innerText
+      .split('')
+      .map((letter: any, idx: any) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+      .join('')
+    })
+
+    console.log('efter ', labels)
+    this.spanEl = this.elRef.nativeElement.querySelectorAll('span');
+    console.log(this.spanEl)
+  }
+
+  onInputClick() {
+    this.spanEl.forEach((span: any) => {
+      span.classList.add('wave-animation');
+    })
+  }
 
 }
