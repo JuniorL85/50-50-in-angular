082b80229cc160d928fdf202b46cc549a0522314
JuniorL85 <albertinolopes@hotmail.com>
2022-12-02 11:51:25 +0100

    sound-board klar samt lagt till ljudfiler

---
+++ src/app/components/sound-board/sound-board.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-sound-board',
+  templateUrl: './sound-board.component.html',
+  styleUrls: ['./sound-board.component.scss']
+})
+export class SoundBoardComponent {
+
+  constructor(private elRef: ElementRef) {}
+
+  playSound(sound: string) {
+    this.stopSounds();
+    this.elRef.nativeElement.querySelector(`#${sound}`).play();
+  }
+
+  stopSounds() {
+    const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+    sounds.forEach(sound => {
+      const song = this.elRef.nativeElement.querySelector(`#${sound}`);
+
+      song.pause()
+      song.currentTime = 0;
+  })
+  }
+
+}
